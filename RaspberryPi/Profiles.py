7e36a11975593f703d8e4f5ecbe622066e4c014e
Natehryan <143719958+Natehryan@users.noreply.github.com>
2023-11-09 12:02:04 -0500

    commit message

--- RaspberryPi/Profiles.py
+++ RaspberryPi/Profiles.py
@@ -1,5 +1,10 @@
 import mysql.connector
 
+actList = ''
+image = ''
+name = ''
+description = ''
+active = True
 class Profiles(id, actList, image, name, description, active):
     def __init__(self, id, actList, image, name, description, active):
         self.id = id
@@ -14,6 +19,30 @@ class Profiles(id, actList, image, name, description, active):
 
     def setActive(active):
         active = True
+    
+    def writeEntry(id, actList, image, name, description, active):
+        #if it is not already there write the entry, otherwise change entry
+        lines = set(open('profiles.txt').readlines())
+        inLine = False
+        newLine = [id, ', ', actList, ', ', image,  ', ', name,  ', ', description,  ', ', active]
+    
+        for line in lines:
+            if str(id) in line:
+                line = line.replace(newLine)
+                inLine = True
+        
+        if inLine == False:
+            with open('profiles.txt', 'a') as f:
+                f.write(newLine)
+                f.write('\n')
+        else: 
+            with open('profiles.txt', 'w') as f:
+                f.write(lines)
+                f.write('\n')
+        
+        
+
+
 
 
 profiledb = mysql.connector.connect(
